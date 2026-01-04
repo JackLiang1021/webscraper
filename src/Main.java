3bc7e88b18d0b1cf926f7b68fb179b561c74ce3f
Jack Liang <jacklian@iu.edu>
2026-01-04 05:37:37 -0500

    Established base test connection to url and parsing page

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        URL url = new URL("https://www.google.com/robots.txt");
+
+
+        URLConnection con = url.openConnection();
+        InputStream is = con.getInputStream();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+
         }
     }
 }

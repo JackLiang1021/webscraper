ad7e75edef413930185036867e2424ad55a962a5
Jack Liang <jacklian@iu.edu>
2026-01-05 06:14:44 -0500

    Fixed and added proper url creation and not string concat

--- src/Link.java
+++ src/Link.java
@@ -3,25 +3,38 @@ import java.net.URL;
 import java.util.Stack;
 
 public class Link {
-    private String url;
+    private String StringUrl;
     private Stack<String> endpoint = new Stack<>();
     private Stack<String> UsedEndpoints = new Stack<>();
+    private URL url;
 
-    public Link(String url){
-        this.url = url;
+    public Link(String url) throws MalformedURLException {
+        this.StringUrl = url;
+        this.url = new URL(url);
     }
 
-    public Link(String url, Stack<String> endpoints){
-        this.url = url;
+    public Link(String url, Stack<String> endpoints) throws MalformedURLException {
+        this.StringUrl = url;
+        this.url = new URL(url);
         this.endpoint = endpoints;
     }
 
     public URL UrlEndNextEndpoint() throws MalformedURLException {
-        UsedEndpoints.push(endpoint.peek());
-        return new URL(this.url + endpoint.pop());
+        String path = endpoint.pop();
+        if (!path.startsWith("/")) path = "/" + path;
+        UsedEndpoints.push(path);
+        return new URL(this.url.getProtocol(), this.url.getHost(), this.url.getPort(), path);
+    }
+
+    public URL GetRobotTxtUrl() throws MalformedURLException {
+        return new URL(this.url.getProtocol(), this.url.getHost(), this.url.getPort(), "/robots.txt");
+    }
+
+    public boolean HasNextEndpoint(){
+        return !endpoint.isEmpty();
     }
     public URL getUrl() throws MalformedURLException {
-        return new URL(this.url);
+        return this.url;
     }
 
     public void AddEndpoint(Stack<String> endpoint){
--- src/Main.java
+++ src/Main.java
@@ -12,33 +12,37 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-//        Stack<String> urls = new Stack<>();
-//
-//        urls.push("https://www.google.com/robots.txt");
-//
-//        while (!urls.empty()) {
-//
-//            URL url = new URL(urls.pop());
-//
-//            URLConnection con = url.openConnection();
-//            InputStream is = con.getInputStream();
-//
-//
-//            try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-//                String line = null;
-//
-//                while ((line = br.readLine()) != null) {
-//                    if(line.contains("Allow: ")) {
-//                        System.out.println(line);
-//                    }
-//                }
-//
-//
-//            }
-//
-//        }
+
+        Stack<String> urls = new Stack<>();
+        urls.push("https://www.google.com");
+
+        while(!urls.isEmpty()){
+            Link link = new Link(urls.pop());
+            Stack<String> endpoints = new Stack<>();
+            link.AddEndpoint(endpoints);
+            URLConnection con;
+            if(link.HasNextEndpoint()){
+                con = link.getUrl().openConnection();
+            }else{
+                con = link.GetRobotTxtUrl().openConnection();
+            }
+
+            InputStream is = con.getInputStream();
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+                String line = null;
+
+                while ((line = br.readLine()) != null) {
+                    if(line.contains("Allow: ")) {
+                        System.out.println(line);
+                    }
+                }
 
 
+            }
+
+
+        }
 
 
     }
